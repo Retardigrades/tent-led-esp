abe1710f570f344c08cf5233faa89fd95c883d67
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-05-18 08:30:39 +0200

    Add first working version
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/example_config.h
+++ src/example_config.h
@@ -1,3 +1,6 @@
+#define HOSTNAME "led_controller"
+#define APP_NAME "led_control"
+
 #define WIFI_SSID "orange-1"
 #define WIFI_PASSWD "xxx"
 
@@ -5,6 +8,8 @@
 
 #define UPDATE_EP "/led_fw"
 #define UPDATE_PORT 6655
+
+#define SYSLOG_PORT 6656
 
 #define BRIGHTNESS 90
 #define STRIPS 5
--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,13 @@
 
 #define FASTLED_ESP8266_D1_PIN_ORDER
 #define FASTLED_ALLOW_INTERRUPTS 0
-//#define FASTLED_INTERRUPT_RETRY_COUNT 0
-//#define INTERRUPT_THRESHOLD 1
 
 #include <ESP8266WiFi.h>
 #include <SPI.h>
 #include <WiFiUdp.h>
 
 #include <FastLED.h>
+#include <Syslog.h>
 
 #include "config.h"
 #include "update.h"
@@ -19,7 +18,28 @@ FASTLED_USING_NAMESPACE
 #define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 #define NUM_LEDS STRIPS *LED_PER_STRIP
-CRGB leds[NUM_LEDS];
+#define BUFSIZE NUM_LEDS * sizeof(CRGB)
+
+// Data of the led
+union {
+  struct {
+    CRGB leds[NUM_LEDS];
+  };
+  unsigned char buffer[NUM_LEDS * sizeof(CRGB)];
+} data;
+
+WiFiUDP udpClient;
+WiFiUDP udpServer;
+Syslog syslog(udpClient, CONTROLLER, SYSLOG_PORT, HOSTNAME, APP_NAME, LOG_KERN);
+
+void init_leds() {
+  CRGB cl[] = {CRGB::Red, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Purple};
+  for (uint8_t i = 0; i < STRIPS; ++i) {
+    for (uint8_t j = 0; j < LED_PER_STRIP; ++j) {
+      data.leds[i * LED_PER_STRIP + j] = cl[i];
+    }
+  }
+}
 
 void setup() {
   // disable sleep mode for better data rate
@@ -30,27 +50,73 @@ void setup() {
     delay(500);
   }
 
+  syslog.log(LOG_INFO, "Controller was (re)booted - check for updates");
+
   UPDATE(CONTROLLER, UPDATE_PORT, UPDATE_EP);
 
+  syslog.log(LOG_INFO, "No updates to handle. init...");
+
   // tell FastLED about the LED strip configuration
-  FastLED.addLeds<WS2811_PORTA, 5>(leds, 20).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2811_PORTA, STRIPS>(data.leds, LED_PER_STRIP)
+      .setCorrection(TypicalLEDStrip);
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
 
-  CRGB cl[] = {CRGB::Red, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Purple};
-  for (uint8_t i = 0; i < STRIPS; ++i) {
-    for (uint8_t j = 0; j < LED_PER_STRIP; ++j) {
-      leds[i * LED_PER_STRIP + j] = cl[i];
-    }
-  }
+  init_leds();
+
+  udpServer.begin(7000);
+
+  syslog.log(LOG_INFO, "Starting normal operation");
 }
 
 void loop() {
-  WiFi.setSleepMode(WIFI_NONE_SLEEP);
+  if (WiFi.status() != WL_CONNECTED) {
+    ESP.restart();
+  }
 
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000 / 50);
+  size_t packetSize = udpServer.parsePacket();
+  if (0 == packetSize) {
+    // Use fastled delay for dithering
+    FastLED.delay(5);
+    return;
+  }
+
+  //  syslog.logf(LOG_DEBUG, "Got packet with size %d...", packetSize);
+
+  unsigned char command = udpServer.read();
+
+  switch (command) {
+  case 0x01: {
+    packetSize -= 1;
+    size_t read = 0;
+    while (read < BUFSIZE) {
+      while (packetSize <= 0) {
+        packetSize = udpServer.parsePacket();
+        if (packetSize == 0)
+          delay(1);
+      }
+
+      size_t current = udpServer.read(data.buffer + read, BUFSIZE - read);
+      packetSize -= current;
+      read += current;
+    }
+    break;
+  }
+  case 0x02:
+    syslog.log(LOG_CRIT, "Reboot controller");
+    delay(50);
+    ESP.restart();
+    break;
+  case 0x03:
+    if (packetSize > 1) {
+      unsigned char brightness = udpServer.read();
+      syslog.logf(LOG_INFO, "brightness: %d", brightness);
+      FastLED.setBrightness(brightness);
+    }
+    break;
+  }
 
   FastLED.show();
+  FastLED.delay(1000 / 80);
 }
