db186710eb6a955457fa1a7fc3ae9f075e0fc7a6
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-05-18 03:03:08 +0200

    Update config
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/config.h
+++ src/config.h
@@ -1,8 +1,11 @@
+#define WIFI_SSID "orange-1"
+#define WIFI_PASSWD "xxx"
+
 #define CONTROLLER "brettchen"
 
 #define UPDATE_EP "/led_fw"
 #define UPDATE_PORT 6655
 
-#define MAX_BRIGHTNESS 90
+#define BRIGHTNESS 90
 #define STRIPS 5
-#define LEDS_PER_STRIP 20
+#define LED_PER_STRIP 20
