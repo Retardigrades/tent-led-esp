0b23262d5ce9921e38dd675cdaf44570130d125a
Jan Losinski <losinski@wh2.tu-dresden.de>
2017-05-19 18:18:41 +0200

    Further testing and fixing
    Signed-off-by: Jan Losinski <losinski@wh2.tu-dresden.de>

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,7 @@ FASTLED_USING_NAMESPACE
 #define NUM_LEDS STRIPS *LED_PER_STRIP
 #define BUFSIZE NUM_LEDS * sizeof(CRGB)
 #define FRAME_TIME 1000 / FRAME_RATE
+#define COUNT_FRAMES_TO 10000
 
 // Data of the led
 union {
@@ -34,6 +35,54 @@ WiFiUDP udpServer;
 WiFiUDP udpControl;
 Syslog syslog(udpClient, CONTROLLER, SYSLOG_PORT, HOSTNAME, APP_NAME, LOG_KERN);
 
+struct {
+  unsigned long last_frame;
+  unsigned long last_packet_check;
+  unsigned long last_control_check;
+} ts;
+
+uint16_t frame_cnt = 0;
+
+void eventWiFi(WiFiEvent_t event) {
+  switch (event) {
+    case WIFI_EVENT_STAMODE_CONNECTED:
+      break;
+
+    case WIFI_EVENT_STAMODE_DISCONNECTED:
+      // Maybe find a better solution for this.
+      ESP.restart();
+      break;
+
+    case WIFI_EVENT_STAMODE_AUTHMODE_CHANGE:
+      break;
+
+    case WIFI_EVENT_STAMODE_GOT_IP:
+      break;
+
+    case WIFI_EVENT_STAMODE_DHCP_TIMEOUT:
+      break;
+
+    case WIFI_EVENT_SOFTAPMODE_STACONNECTED:
+      break;
+
+    case WIFI_EVENT_SOFTAPMODE_STADISCONNECTED:
+      break;
+
+    case WIFI_EVENT_SOFTAPMODE_PROBEREQRECVED:
+      break;
+  }
+}
+
+void netSetup() {
+  WiFi.onEvent(eventWiFi);
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+
 void init_leds() {
   // tell FastLED about the LED strip configuration
   FastLED.addLeds<WS2811_PORTA, STRIPS>(data.leds, LED_PER_STRIP)
@@ -45,6 +94,7 @@ void init_leds() {
   // We do not use dithering for now.
   FastLED.setDither(0);
 
+#ifdef DEBUG
   // Set an initial color to the channel for debuggung
   CRGB cl[] = {CRGB::Red, CRGB::Blue, CRGB::Green, CRGB::Yellow, CRGB::Purple};
   for (uint8_t i = 0; i < STRIPS; ++i) {
@@ -53,22 +103,23 @@ void init_leds() {
     }
   }
   FastLED.show();
+#endif
 }
 
 void setup() {
+#ifdef DEBUG
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+#endif
+
   // disable sleep mode for better data rate
   WiFi.setSleepMode(WIFI_NONE_SLEEP);
 
-  WiFi.begin(WIFI_SSID, WIFI_PASSWD);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
+  netSetup();
 
   syslog.log(LOG_INFO, "Controller was (re)booted - check for updates");
 
   UPDATE(CONTROLLER, UPDATE_PORT, UPDATE_EP);
-
-  syslog.log(LOG_INFO, "No updates to handle. init...");
 
   init_leds();
 
@@ -76,15 +127,6 @@ void setup() {
   udpControl.begin(7001);
 
   syslog.log(LOG_INFO, "Starting normal operation");
-}
-
-void check_wifi() {
-  if (WiFi.status() != WL_CONNECTED) {
-    syslog.log(LOG_CRIT, "Lost WIFI connection ... Reboot controller");
-    delay(50);
-
-    ESP.restart();
-  }
 }
 
 void check_control() {
@@ -95,18 +137,18 @@ void check_control() {
 
     switch (command) {
       case 0x01:
-        syslog.log(LOG_CRIT, "Reboot controller");
-        delay(50);
-        ESP.restart();
-        break;
+	syslog.log(LOG_CRIT, "Reboot controller");
+	delay(50);
+	ESP.restart();
+	break;
       case 0x02:
-        if (packetSize > 1) {
-          unsigned char brightness = udpControl.read();
-          syslog.logf(LOG_INFO, "brightness: %d", brightness);
-          FastLED.setBrightness(brightness);
-          delay(10);
-        }
-        break;
+	if (packetSize > 1) {
+	  unsigned char brightness = udpControl.read();
+	  syslog.logf(LOG_INFO, "brightness: %d", brightness);
+	  FastLED.setBrightness(brightness);
+	  delay(10);
+	}
+	break;
     }
   }
 }
@@ -118,58 +160,57 @@ bool check_server(unsigned long loop_time) {
     return false;
   }
 
-  //  syslog.logf(LOG_DEBUG, "Got fist framepart packet at %dms...", loop_time);
   size_t read = 0;
-  uint8_t packet_cnt = 0;
 
   while (read < BUFSIZE) {
     while (packetSize <= 0) {
       packetSize = udpServer.parsePacket();
       if (packetSize == 0) {
-        if (millis() - loop_time > (FRAME_TIME / 3)) {
-          syslog.logf(LOG_DEBUG, "Did not get %d bytes in time, drop frame...",
-                      BUFSIZE - read);
-          return false;
-        }
-        delay(1);
+	if (millis() - loop_time > (FRAME_TIME / 3)) {
+	  syslog.logf(LOG_DEBUG,
+		      "Did not get %d bytes in "
+		      "time, drop frame...",
+		      BUFSIZE - read);
+	  return false;
+	}
+	delay(1);
       }
     }
-    //    syslog.logf(LOG_DEBUG, "Process framepart packet %d with %d bytes at
-    //    %d ms...", ++packet_cnt, packetSize, millis());
 
     size_t current = udpServer.read(data.buffer + read, BUFSIZE - read);
     packetSize -= current;
     read += current;
   }
 
-  //  syslog.logf(LOG_DEBUG, "Write frame to LED at %d ms...", millis());
   FastLED.show();
-  //  syslog.logf(LOG_DEBUG, "Frame to LED at %d ms...", millis());
   return true;
 }
 
-struct {
-  unsigned long last_frame;
-  unsigned long last_packet_check;
-  unsigned long last_control_check;
-} ts;
 
 void loop() {
   unsigned long now = millis();
 
   if (now - ts.last_control_check > 50) {
-    check_wifi();
     check_control();
     ts.last_control_check = now;
   }
-  if (now - ts.last_frame < FRAME_TIME / 2) {
+
+  if ((now - ts.last_frame < FRAME_TIME / 2) ||
+      (now - ts.last_packet_check < FRAME_TIME / 10)) {
     return;
   }
-  if (now - ts.last_packet_check < FRAME_TIME / 10) {
-    return;
-  }
+
   if (check_server(now)) {
+
+#ifdef COUNT_FRAMES_TO
+    if (0 == (++frame_cnt % COUNT_FRAMES_TO)) {
+      syslog.logf(LOG_DEBUG, "%d Frames written to LED", COUNT_FRAMES_TO);
+      frame_cnt = 0;
+    }
+#endif
+
     ts.last_frame = now;
   }
+
   ts.last_packet_check = now;
 }
